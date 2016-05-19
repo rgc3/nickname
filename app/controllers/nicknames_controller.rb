86dc701ac454690d2069004c84e2556990c05af0
Robert Cunningham <rgc3dev@gmail.com>
2016-05-19 16:06:26 -0400

    Complete final tweaks for functionality in nicknames controller with corresponding code in views.

--- app/controllers/nicknames_controller.rb
+++ app/controllers/nicknames_controller.rb
@@ -6,19 +6,29 @@ class NicknamesController < ApplicationController
 # The new method could make available to the view a @nickname instance variable by creating a new Nickname:
   def new
     @nickname = Nickname.new
+    @nicknames = Nickname.all
   end
 
-  def nickname_params
-   params.require(:nicknames).permit(:n_name)
-  end
-
-# The first line creates a new instance variable called @nickname that holds a Nickname object built from the data, the user submitted. The nickname_params method is used to collect all the fields from object :nicknames. The data was passed from the new method to create using the params object.
+  # This action uses POST parameters. They are most likely coming
+  # from an HTML form which the user has submitted. The URL for
+  # this RESTful request will be "/clients", and the data will be
+  # sent as part of the request body.
   def create
     @nickname = Nickname.new(nickname_params)
+    if @nickname.save
+      redirect_to root_path
+    else
+      # This line overrides the default rendering behavior, which
+      # would have been to render the "create" view.
+      render "new"
+    end
   end
 
-  def list
-    @nicknames = Nickname.all
+  private
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def nickname_params
+   params.require(:nickname).permit(:n_name)
   end
 
 end
